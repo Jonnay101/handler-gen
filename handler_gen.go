e23597b1ce4ece1591ed17b959265f5ed25c9df5
John Hughes <john.hughes@musictribe.com>
2022-05-12 08:49:13 +0100

    test: :white_check_mark: Pass nil value binding interface test
    add a nil check on the binding interface

--- handler_gen.go
+++ handler_gen.go
@@ -1,1 +1,30 @@
 package handlergen
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type DomainLogicHandler func(i interface{}) (responseData interface{}, statusCode int, err error)
+
+func EchoHandleFuncGenerator(dFn DomainLogicHandler, i interface{}) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		if i == nil {
+			return echo.NewHTTPError(
+				http.StatusBadRequest,
+				errors.New("Binding interface has a nil value"),
+			)
+		}
+		if err := c.Bind(i); err != nil {
+			return echo.NewHTTPError(
+				http.StatusBadRequest,
+				fmt.Errorf("Error while binding request >>>> \n%w\n", err),
+			)
+		}
+
+		return nil
+	}
+}
